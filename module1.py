d6f90f4a9243f7e153323011ae666b1dcd5f5863
Micro Mac <micromac@Micro-Mac.local>
2019-06-29 09:27:33 +1000

    testing vs code

--- module1.py
+++ module1.py
@@ -1,5 +1,17 @@
 # Module 1 Risk and Return of an Individual Asset
+import fix_yahoo_finance as yf
+import pandas_datareader.data as pdr
+
 print("Individual Asset Risk and Return")
 exchange = '.AX'
+exchange_code = "^AXJO"
 print('Enter your ' + exchange + ' stock....')
 stock = input() + exchange
+
+
+def history(stock):
+    recent_history = pdr.get_data_yahoo(stock)
+    print(recent_history.tail())
+
+
+history(stock)
