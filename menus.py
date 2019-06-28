f5ff8373b6bdd5a179a5e151708e5a67b230d5b3
Micro Mac <micromac@Micro-Mac.local>
2019-06-29 07:52:40 +1000

    Initial Commit

---
+++ menus.py
@@ -0,0 +1,26 @@
+border = '-' * 20
+def menus():
+    print(border + 'MENU' + border)
+    print('1. Risk/Return of Individual Asset')
+    print('2. Efficient Frontier')
+    print('3. Capital Asset Pricing Model')
+    print('4. Recent High/Low Open/Close')
+    print(border*2+'----')
+
+menu = True
+while menu:
+    menus()
+    key = int(input('Input your choice; '))
+
+    if key == 1:
+        print('Enter your stock...')
+
+    elif key == 2:
+        import module2
+
+    elif key == 3:
+        print('Hello 3')
+    elif key == 4:
+        import module4
+    else:
+        print('Input invalid try again.')
---
+++ module4.py
@@ -0,0 +1,23 @@
+# MODULE 4 - Recents
+import fix_yahoo_finance as yf
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import pandas_datareader.data as pdr
+
+yf.pdr_override()
+
+exchange = '.AX'
+print('Enter your ' + exchange + ' stock....')
+stock = input() + exchange
+while stock != 'Exit':
+    recent_history = pdr.get_data_yahoo(stock)
+    print(recent_history.tail())
+    recent_history.tail()['Adj Close'].plot()
+    plt.title('Share Price vs Date')
+    plt.ylabel('Return')
+    plt.xlabel('Date')
+    plt.show()
+    print('Enter your ' + exchange + ' stock....')
+    stock = input() + exchange
