3587e01a96759452e47caac2c77e24f0d912ffa9
Micro Mac <micromac@Micro-Mac.local>
2019-07-01 09:53:20 -0800

    Matrices

--- PortfolioMatrices.py
+++ PortfolioMatrices.py
@@ -1,7 +1,6 @@
-# Portfilo Matrices
+# Portfilo Matrices     #don't forget to add stock prices and how many to purchase given weights
 import numpy as np
 stocks = ['CBA.AX', 'WOW.AX', 'BHP.AX', 'A2M.AX']
-noa = len(stocks)
 ones = []
 for i in stocks:
     ones.append(1)
