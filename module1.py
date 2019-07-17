9810272f4b9affaa99515dd173e253f143cfd434
Micro Mac <micromac@Micro-Mac.local>
2019-07-17 14:28:59 +1000

    Updates

--- module1.py
+++ module1.py
@@ -1,6 +1,7 @@
 # Module 1 Risk and Return of an Individual Asset
 import fix_yahoo_finance as yf
 import pandas_datareader.data as pdr
+import numpy as np
 
 print("Individual Asset Risk and Return")
 print('Exchanges to Choose From:')
@@ -13,10 +14,21 @@ stock = input() + exchange
 
 exchange_code = "^AXJO"
 
+exchange_data = pdr.get_data_yahoo(exchange_code)['Adj Close']
+stock_data = pdr.get_data_yahoo(stock)['Adj Close']
 
-def history(stock):
-    recent_history = pdr.get_data_yahoo(stock)
-    print(recent_history.tail())
+# Beta Calculation
+# beta asset = cov (asset return and market return ) / market variance
+exchange_variance = int(
+    mean(np.log(exchange_data / exchange_data.shift(1)))*252)
+stock_return = mean(np.log(stock_data / stock_data.shift(1)))*252)
+covariance=cov(exchange_return, stock_return)
+exchange_variance=var(exchange_data)
+asset_beta=covariance / exchange_variance
+Print("The beta of the asset is:" + asset_beta)
 
 
-history(stock)
+Print('Are you finished?')
+menu_status=input()
+if menu_status=["yes", 'Yes', 'ye', 'eys']:
+    import menus
--- module2.py
+++ module2.py
@@ -1,4 +1,5 @@
-#MODULE 3 - Markowitz
+# MODULE 3 - Markowitz
+import scipy.optimize as sco
 import numpy as np
 import pandas as pd
 import pandas_datareader.data as pdr
@@ -16,10 +17,10 @@ for ticker in tickers:
     data[ticker] = pdr.get_data_yahoo(ticker)['Adj Close']
 
 rets = np.log(data / data.shift(1))
-rets.hist(bins = 40, figsize=(10,8))
+rets.hist(bins=40, figsize=(10, 8))
 retsmean = rets.mean() * 252
 retscov = rets.cov() * 252
-
+print(retsmean)
 weights = np.random.random(noa)
 weights /= np.sum(weights)
 print('Return of the asset')
@@ -31,12 +32,15 @@ varp = np.dot(weights.T, np.dot(rets.cov() * 252, weights))
 def port_ret(weights):
     return np.sum(rets.mean() * weights) * 252
 
+
 def port_vol(weights):
     return np.sqrt(np.dot(weights.T, np.dot(rets.cov() * 252, weights)))
-#Portfolio Randomiser, Monte Carlo
+
+
+# Portfolio Randomiser, Monte Carlo
 prets = []
 pvols = []
-for p in range (2500):
+for p in range(2500):
     weights = np.random.random(noa)
     weights /= np.sum(weights)
     prets.append(port_ret(weights))
@@ -46,49 +50,54 @@ prets = np.array(prets)
 pvols = np.array(pvols)
 
 
-#Minimisation
-import scipy.optimize as sco
+# Minimisation
+
+
 def min_func_sharpe(weights):
     return -port_ret(weights) / port_vol(weights)
 
-cons = ({'type': 'eq', 'fun': lambda x: np.sum(x) -1})
-bnds = tuple((0,1) for x in range(noa))
-eweights = np.array(noa * [1. / noa,])
+
+cons = ({'type': 'eq', 'fun': lambda x: np.sum(x) - 1})
+bnds = tuple((0, 1) for x in range(noa))
+eweights = np.array(noa * [1. / noa, ])
 min_func_sharpe(eweights)
 print('Optimal Weights')
 print(eweights)
 
-opts = sco.minimize(min_func_sharpe, eweights, method ='SLSQP', bounds=bnds, constraints = cons)
+opts = sco.minimize(min_func_sharpe, eweights,
+                    method='SLSQP', bounds=bnds, constraints=cons)
 
 opts['x'].round(3)
 port_ret(opts['x']).round(3)
 port_vol(opts['x']).round(3)
 port_ret(opts['x']) / port_vol(opts['x'])
 
-optv = sco.minimize(port_vol, eweights, method = 'SLSQP', bounds = bnds, constraints = cons)
+optv = sco.minimize(port_vol, eweights, method='SLSQP',
+                    bounds=bnds, constraints=cons)
 optv['x'].round(3)
 port_ret(optv['x']).round(3)
 port_vol(optv['x']).round(3)
 port_ret(optv['x']) / port_vol(optv['x'])
 
-#Efficient Frontier
+# Efficient Frontier
 cons = ({'type': 'eq', 'fun': lambda x: port_ret(x) - tret},
-{'type':'eq', 'fun':lambda x: np.sum(x) - 1})
+        {'type': 'eq', 'fun': lambda x: np.sum(x) - 1})
 
-bnds = tuple((0,1) for x in weights)
+bnds = tuple((0, 1) for x in weights)
 
 trets = np.linspace(0.05, 0.2, 50)
 tvols = []
 for tret in trets:
-    res = sco.minimize(port_vol, eweights, method = 'SLSQP', bounds = bnds, constraints = cons)
+    res = sco.minimize(port_vol, eweights, method='SLSQP',
+                       bounds=bnds, constraints=cons)
     tvols.append(res['fun'])
 tvols = np.array(tvols)
 
 
 plt.figure(figsize=(10, 6))
 plt.scatter(pvols, prets, c=prets / pvols, marker='o', cmap='coolwarm')
-plt.plot(port_vol(opts['x']), port_ret(opts['x']), 'b*', markersize = 15.0)
-plt.plot(port_vol(optv['x']), port_ret(optv['x']), 'r*', markersize = 15.0)
+plt.plot(port_vol(opts['x']), port_ret(opts['x']), 'b*', markersize=15.0)
+plt.plot(port_vol(optv['x']), port_ret(optv['x']), 'r*', markersize=15.0)
 plt.xlabel('expected volatility')
 plt.ylabel('expected return')
 plt.colorbar(label='Sharpe ratio')
