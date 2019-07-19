556573a25f9169faba8276fa4d35868a6ff89af9
Micro Mac <micromac@Micro-Mac.local>
2019-07-19 19:11:34 +1000

    Beginning Weight Vector

--- PortfolioMatrices.py
+++ PortfolioMatrices.py
@@ -2,12 +2,24 @@
 import numpy as np
 import fix_yahoo_finance as yf
 import pandas_datareader as pdr
-stocks = ['CBA.AX', 'WOW.AX', 'BHP.AX', 'A2M.AX']
+
+stocks = ['CBA.AX', 'WOW.AX', 'BHP.AX', 'A2M.AX', 'MQG.AX',
+          'BOQ.AX', 'NAB.AX', 'CTX.AX', 'FMG.AX', 'A40.AX']
+# ONE VECTOR
 ones = []
 for i in stocks:
     ones.append(1)
 print(ones)
 
+# RETURN VECTOR
 stock_data = pdr.get_data_yahoo(stocks)['Adj Close']
 retvect = np.mean(np.log(stock_data / stock_data.shift(1))*252).tolist()
 print(retvect)
+
+# WEIGHT VECTOR
+weights = []
+for i in stocks:
+    weights.np.random()
+
+
+# COVARIANCE MATRIX
