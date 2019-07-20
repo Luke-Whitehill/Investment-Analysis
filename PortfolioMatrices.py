55a24f5175add48556e899ecbb3216431ab9d033
Micro Mac <micromac@Micro-Mac.local>
2019-07-20 10:33:34 +1000

    Trying Scalars

--- PortfolioMatrices.py
+++ PortfolioMatrices.py
@@ -3,18 +3,19 @@ import numpy as np
 import fix_yahoo_finance as yf
 import pandas_datareader as pdr
 
-stocks = ['CBA.AX', 'WOW.AX', 'BHP.AX', 'A2M.AX', 'MQG.AX',
-          'BOQ.AX', 'NAB.AX', 'CTX.AX', 'FMG.AX', 'A40.AX']
+stocks = ['CBA.AX', 'WOW.AX', 'BHP.AX']
 # ONE VECTOR
 ones = []
 for i in stocks:
     ones.append(1)
+one = np.array(ones)
 print('Ones')
-print(ones)
+print(one)
 
 # RETURN VECTOR
 stock_data = pdr.get_data_yahoo(stocks)['Adj Close']
-retvect = np.mean(np.log(stock_data / stock_data.shift(1))*252).tolist()
+ret = np.log(stock_data / stock_data.shift(1))
+retvect = np.array((np.mean(ret)*252))
 print('Return Vector')
 print(retvect)
 
@@ -25,3 +26,18 @@ print('Weights Vector')
 print(weights)
 
 # COVARIANCE MATRIX
+retcov = np.array(ret.cov()*252)
+invretcov = np.matrix(retcov).I
+print('Variance Co-Variance Matrix')
+print(retcov)
+print(invretcov)
+
+# A CALCUATION
+A = np.dot(one, np.dot(invretcov, np.transpose(one)))
+print('A')
+print(A)
+
+# B CALCULATION
+B = np.dot(one, np.dot(invretcov, retvect))
+print('B')
+print(B)
